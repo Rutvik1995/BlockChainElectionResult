13b1afa8d70e8a5183505c0fd7618672bf2bb647
Rutvik <rutvikpatel.rm@gmail.com>
2019-09-05 21:38:15 -0700

    smoke test

---
+++ contracts/Election.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+contract Election {
+
+    //Model the candidate
+    //Store the candidate
+    //Fetch the candidate
+    //Store candidate count
+
+    string public candidate;
+
+
+    //string public candidate;
+
+    //Constructor
+    constructor() public{
+        candidate = "Candidate 1";
+    }
+}
