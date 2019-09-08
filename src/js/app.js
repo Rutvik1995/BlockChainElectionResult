53c6dce22abe185661b41b3e529732417b488c37
Rutvik <rutvikpatel.rm@gmail.com>
2019-09-08 16:07:53 -0700

    Listing the account and candidate in UI

--- src/js/app.js
+++ src/js/app.js
@@ -14,13 +14,16 @@ App = {
      */
     if(typeof web3 !=='undefined'){
       
-      App.web3Provider =web3.currentProvider;
-      web3 = new Web3(web3.currentProvider);
+    //   App.web3Provider =web3.currentProvider;
+    //   web3 = new Web3(web3.currentProvider);
+    //   web3.eth.getAccounts().then(console.log);
      
-    }
-    else{
+    // }
+    // else{
       App.web3Provider= new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
+      console.log(web3.eth.accounts);
+ 
     }
 
     return App.initContract();
@@ -34,6 +37,8 @@ App = {
      $.getJSON("Election.json",function(election){
         App.contracts.Election = TruffleContract(election);
         App.contracts.Election.setProvider(App.web3Provider);
+        console.log( App.contracts);
+        console.log(App.contracts.Election);
         return App.render();
      })
   },
@@ -44,11 +49,16 @@ render:function(){
   var content = $("#content");
  // loader.show();
  // content.hide();
+ 
 
-web3.eth.getCoinbase(function(err,account){
+
+
+web3.eth.getAccounts(function(err,account){
+  console.log(account);
   if(err===null){
-    App.account = account;
-    $("#accountAddress").html("Your account: "+account);
+    App.account = account[0];
+    console.log(App.account[0]);
+    $("#accountAddress").html("Your account: "+account[0]);
   }
 });
 
@@ -73,7 +83,6 @@ for(var i=1;i<=candidateCount;i++){
 
   console.log("after function");
 }
-
 //loader.hide();
 //loader.show();
 
@@ -105,7 +114,7 @@ for(var i=1;i<=candidateCount;i++){
   //    * Replace me...
   //    */
   // }
-
+ 
 };
 
 $(function() {
